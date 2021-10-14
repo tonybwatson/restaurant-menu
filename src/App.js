f0cb4fc3725e19efafb9f669f079a2bccf4cbdfa
tonybwatson <tonybwatson@gmail.com>
2021-10-14 11:48:22 -0400

    api data writing to page - kind of

--- src/App.js
+++ src/App.js
@@ -1,16 +1,92 @@
+import React, { Component } from 'react';
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+import MenuItem from './MenuItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            // currentItem: "",
+            awesomeApiData: {},
+            // items: [],
+        }
+    }
+
+    componentDidMount() {
+        console.log("App successfully Loaded!")
+        this.loadAPI();
+    }
+    componentDidUpdate() {
+        console.log("App successfully Updated!")
+        // if (!this.state.awesomeApiData.length) {
+        //     this.loadAPI()
+        // }
+    }
+
+    loadAPI() {
+        // Make a request
+        axios.get('https://port-3000-aincbootcampapi-ianrios529550.codeanyapp.com/api/menu/type/5')
+            .then(response => {
+                // handle success
+                console.log(response.data);
+                this.setState({
+                    awesomeApiData: response.data
+                })
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
+
+    render() {
+        return (
+            <>
+
+            {/* ? ensures this.state.awesomeApiData exists before trying to get name*/}
+                <p>{this.state.awesomeApiData[0]?.name}</p>
+                <div className="container text-center">
+                    <div className="row">
+                        <div className="container"></div>
+                        <div className="col">
+                            <h1 className="row pt-4">The Awesome Cafe</h1>
+                            <h5 className="row">348 E Main St, Lexington KY </h5>
+                            <h5 className="row">Open daily 12pm-8pm</h5>
+                        </div>
+                    </div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                            </div>
+                            <div className="col">
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                            </div>
+                            <div className="col">
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                                <MenuItem />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <p>{this.state.awesomeApiData}</p> */}
+            </>
+        );
+    }
 }
-
 export default App;
---
+++ src/pseudocode.md
@@ -0,0 +1,19 @@
+push to github to save, not to host
+
+dynamically render when info pulled from api
+
+How to get data with more than one call?
+
+set prices - randomize? - math.random or choose price based on number of letters in food name?
+
+max 60 calls per minute on api
+
+
+Objective - Display menu for restaurant, including name, address, hours of operation, and at least 15 menu items from API with chosen prices for items.
+
+Start
+    INIT
+        Create Items
+
+    Render "constant" info to page - heading, hours, etc.
+    Render Items to page - 3 rows of 5 - 3 api calls?
